3656ec37e0e6117ce24af67c8bd6b63786e4d02e
zhinuanguo <zhinuanguo@softwareimprovementgroup.com>
2025-05-21 16:57:30 +0200

    cancel time sleep

--- src/llmperf/rag_evaluation_clients.py
+++ src/llmperf/rag_evaluation_clients.py
@@ -188,7 +188,6 @@ def run_batch(model: str,
             llm_api=llm_api,
             output_dir=output_dir,
             rerun=rerun)
-    time.sleep(30)
     run_job(model=model, filename=prod_filename, dataset=prod_data,
             additional_sampling_params=additional_sampling_params,
             num_concurrent_requests=num_concurrent_requests,
